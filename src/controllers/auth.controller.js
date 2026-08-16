3adf1b06a032e45af597a444561599acff1f7488
Ekagr Khanna <ekagrkhannaind120@gmail.com>
2026-08-16 10:03:38 +0530

    401 fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -57,6 +57,7 @@ const login = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Login successful",
+      token,
       user: {
         id: user._id,
         username: user.username,
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,14 @@ const User = require("../models/user.model");
 
 const protect = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    let token = req.cookies?.token;
+
+    if (!token && req.headers.authorization) {
+      const authHeader = req.headers.authorization;
+      if (authHeader.startsWith("Bearer ")) {
+        token = authHeader.slice(7);
+      }
+    }
 
     if (!token) {
       return res.status(401).json({
